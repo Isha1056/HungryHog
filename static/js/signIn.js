f36dfd0c2a0f81e74798534967c8d852f14066c6
Rishabh <rishabhbhatia57@gmail.com>
2023-03-27 00:21:36 +0530

    signIn page and admin kitchen list

--- static/js/signIn.js
+++ static/js/signIn.js
@@ -1,34 +1,34 @@
 function getUserSignInDetails() {
-    console.log("ready");
-    let email = document.getElementById("user-email").innerHTML;
-    console.log(email);
+  console.log("ready");
+  let email = document.getElementById("user-email").innerHTML;
+  console.log(email);
 }
 
-
 $(document).ready(function () {
-    jQuery.support.cors = true;
-    $(".user_signin_form").submit(function (e) {
-        e.preventDefault();
-        let email = document.getElementById("email").value;
-        let password = document.getElementById("password").value;
-        console.log(email, password);
+  jQuery.support.cors = true;
+  $(".user_signin_form").submit(function (e) {
+    e.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    console.log(email, password);
 
-        $.ajax({
-            url: "http://127.0.0.1:5000/UsersAuthentication",
-            method: "POST",
-            dataType: "json",
-            data: JSON.stringify({ 
-                'USER_EMAIL': 'abc@gmail.com', 
-                'USER_PASSWORD': 'Pass@123' 
-            }),
-            success: function(response) {
-              console.log(response)
-            },
-            error: function(xhr, status, error) {
-              console.log(xhr, status, error);
-            }
-          });
+    var senddata = {
+      USER_EMAIL: "abc@gmail.com",
+      USER_PASSWORD: "Pass@123",
+    };
+
+    $.ajax({
+      url: "http://127.0.0.1:5000/UsersAuthentication",
+      method: "POST",
+      dataType: "json",
+      contentType: "application/json",
+      data: JSON.stringify(senddata),
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (xhr, status, error) {
+        console.log(xhr, status, error);
+      },
     });
+  });
 });
-
-
